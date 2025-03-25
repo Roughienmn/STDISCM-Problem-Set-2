91c7e9c883aea061306099e111db1eb99e4500d6
Roughienmn <71823612+Roughienmn@users.noreply.github.com>
2025-03-25 23:10:43 +0800

    Add project files.

---
+++ STDISCM Problem Set 2/Dungeon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Dungeon
+{
+    private uint id;
+    private State state;
+    private Thread thread;
+    enum State
+    {
+        EMPTY,
+        ACTIVE
+    }
+
+    public Dungeon(uint id)
+    {
+        this.id = id;
+        this.state = State.EMPTY;
+        this.thread = new Thread(() => this.Run());
+    }
+}
